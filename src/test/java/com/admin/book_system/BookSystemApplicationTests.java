d4415635198541c3160620be8fede2c9d31c25ac
HyungSeok Park <dnflekf2748@gmail.com>
2023-06-20 19:43:01 +0900

    first commit

---
+++ src/test/java/com/admin/book_system/BookSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.book_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
